50bd0e9cbb79a99a3811041675964b40233f737b
Erik Blankendaal <erikb@backbase.com>
2014-01-17 15:19:37 +0100

    made Beatfreak a lot faster

--- players/Beatfreak.groovy
+++ players/Beatfreak.groovy
@@ -3,15 +3,15 @@ package players
 class Beatfreak {
 	static descr = 'Rhythm is everything'
 
+	static freq = [0, 0, 0]
+
 	static hand = { myhands, yourhands ->
-		if (yourhands.size() == 0) 0 else {
-			def freq = [0, 1, 2].collect({ hand->
-				yourhands
-					.findAll({ x ->
-						x == hand
-					})
-					.size()
-			})
+		if (yourhands.size() == 0) {
+			// reset counters
+			freq = [0, 0, 0]
+			0
+		} else {
+			freq[yourhands.last()] += 1
 
 			if (freq[0] > freq[1] & freq[0] > freq[2]) 1 else
 			if (freq[1] > freq[0] & freq[1] > freq[2]) 2 else
